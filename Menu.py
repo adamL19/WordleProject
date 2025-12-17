43c6f85b99059133fe699142ac05a4ea56904338
adamL19 <adamlocke2025@gmail.com>
2025-12-17 00:33:59 -0500

    Updated menu function under Wordle Unlimited and began word checking function

--- Menu.py
+++ Menu.py
@@ -13,10 +13,28 @@ def __main__():
         if answer == '3':
             print("\nBye!")
             break
+
         elif answer == '2':
             w = Wordle()
+
+            while True:
+                result = w.startGame()
+                #updates based on return value of startGame() [not finished]
+                if result == "win":
+                    w.addToStreak()
+                    w.updateHighscore()
+                elif result == "lose":
+                    w.resetStreak()
+                    w.printHighscore()
+                elif result is None:
+                    w.printHighscore()
+                    break
+
+            answer = welcome()
+
         elif answer == '1':
             pass
+        
         else:
             print("Please choose an option from the menu")
 
--- Wordle.py
+++ Wordle.py
@@ -1,44 +1,83 @@
 import random
 
-class InvalidWord(Exception):
+class InvalidWordException(Exception):
     pass
-class LongWord(Exception):
+
+class LongWordException(Exception):
     pass
 
 class Wordle:
     HighestStreak = 0
     def __init__(self):
         self.streak = 0
-        self.answer = None
+        self.solution = Wordle.generateSolution(self)
 
-    def validGuess(self, guess):
+    def addToStreak(self):
+        self.streak += 1
+    
+    def resetStreak(self):
+        self.streak = 0
+    
+    def updateHighscore(self):
+        if self.streak > self.HighestStreak:
+            self.HighestStreak = self.streak
+        
+    def printHighscore(self):
+        print(f"Highest streak: {self.HighestStreak}")
+    
+    #checks if word is long OR in word list 
+    def validGuess(self, guess): 
         with open ("valid_guesses.csv", 'r') as f:
             lines = f.read().splitlines()
             if guess not in lines:
-                raise InvalidWord("Use a real word")
+                raise InvalidWordException("Use a real word")
             if len(guess) > 5:
-                raise LongWord("Use a 5 letter word")
-        return guess
+                raise LongWordException("Use a 5 letter word")
 
-    def generateSolution(self): #reads data and pickes a valid solution
+    #reads data and pickes a valid solution
+    #returns a LIST
+    def generateSolution(self): 
         with open("valid_solutions.csv", 'r') as f:
             lines = f.read().splitlines()
             self.answer = list(lines[random.randrange(0, len(lines)+1)])
     
+    #will run 6 times, one for each guess
+    #generates the solution
+    #returns win to add to streak in menu
+    #restarts lost to reset streak in menu
     def startGame(self):
-        while True:
-            print("To end game, type q or quit")
+        print("To go to menu, type menu")
+        spaces = ["__",  "__",  "__",  "__",  "__"]
+        print(spaces)
+        for i in range(0,7):
             guess = input("\n\nYour guess: ")
-            if guess == 'q' or guess == 'quit':
+            if guess == 'menu':
                 break #Add score streak here maybe?
             guess = list(guess)
     
-    def singleGame(self, solution):
-        solution = Wordle.generateSolution()
-        print("__  __  __  __  __")
-        for i in random.randint(0,5):
-            x = list(input("\nPut guess here: "))
-            if Wordle.validGuess(x) != x:
-                x = list(input("\nPut guess here: "))
+    #check guess with solution
+    #creates a list of right letters wrong spots
+    #creates dict of right letters right spots
+    def singleCheck(self, input):
+        wrongSpot = []
+        rightSpot = {}
+        try:
+            Wordle.validGuess(input)
+        except InvalidWordException as e:
+            print("Error:", e)
+        except LongWordException as e:
+            print("Error:", e)
+        
+        #check for proper position first (green)
+        #can probably be written with an inside loop
+        x = 0
+        for i in self.solution:
+            if i == input[x]:
+                rightSpot[input[x]] = x
+            x += 1
+            
+            
+            
+            
 
         
